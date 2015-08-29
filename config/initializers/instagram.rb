cd08146508efc2d6c198bf376f3c8ee876817edd
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2015-08-29 01:35:35 +0000

    erased the token key

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,5 +1,5 @@
 require "instagram"
     Instagram.configure do |config|
-    config.client_id = "d46a050381c342eabbb8fd095e5f8bc5"
-    config.client_secret = "ef2038a8baf9406e82762f3e08e6bce6"
+    config.client_id = "**********"
+    config.client_secret = "**********"
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '893933960645723', 'ed2dc4513f4192670952481ee4b75f31'
+  provider :facebook, '**********', '**********'
 end
